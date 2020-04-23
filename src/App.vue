b0deb407ec10385268726fdc4096efc1c83f841c
Nailton Sousa <nailton.osn@gmail.com>
2020-04-23 10:58:20 -0300

    Progress bar article

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <header>
+        <h1>Scroll progress bar</h1>
+      </header><!-- /header -->
+      <Progressbar :progress="progress"/>
+      <Post  @progressUpdate="setProgress"/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Post from './components/Post';
+import Progressbar from './components/Progressbar';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Post,
+    Progressbar
+  },
+  data () {
+    return {
+      progress: 0
+    }
+  },
+  methods: {
+    setProgress (progress) {
+      this.progress = progress;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'montseratt', sans-serif;
+}
+main{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+header{
+  background-color: #313131;
+  height: 60px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+header h1{
+  color: #fff;
+font-size: 28px;
+text-align: center;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,75 @@
+<template>
+  <article>
+    <div class="content">
+      <h2>O título do artigo</h2>
+      <h3>Inscreva-se no canal</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis rerum tenetur velit sit, possimus obcaecati repellat porro quisquam facilis, eveniet ratione nulla ad rem excepturi magni consectetur ipsum fugit aliquid non similique expedita. Voluptas, aliquid assumenda dolores facere expedita facilis quas nemo nobis, eveniet, delectus nam voluptatibus. Nisi, tenetur architecto.</p>
+      <p>Temporibus totam facere magnam est optio sequi neque maxime, inventore voluptas unde vel commodi minus sint dolorem sed ducimus cumque asperiores explicabo numquam quaerat minima autem voluptatem accusantium culpa. Accusantium, laborum asperiores placeat voluptatem quasi ab vel laudantium dolores cumque consequatur perspiciatis, ad libero natus alias! Officia laudantium, iusto quo?</p>
+      <p>Ipsam beatae eum nemo numquam commodi vel perspiciatis voluptatem laudantium neque cum officia modi cumque, repudiandae, quisquam ad illo aperiam dolorem laboriosam deleniti accusamus quaerat quibusdam totam quasi! Nam ut cupiditate ex distinctio quibusdam illo assumenda doloribus expedita incidunt, ipsa, itaque nesciunt tempora esse consectetur tempore! Aut nam ducimus, natus?</p>
+      <p>Modi iusto ducimus optio velit non, corporis, similique ipsum, inventore incidunt nostrum natus, amet! Facilis animi libero hic eaque? Cupiditate excepturi consequatur voluptatum amet ipsum quis repellat fugit, fugiat eos, eius deleniti inventore laboriosam reiciendis, vero laudantium? Beatae omnis ex accusantium. Nobis atque earum beatae necessitatibus, saepe rem laboriosam aspernatur.</p>
+      <p>Nesciunt vero illo dolore pariatur sapiente ad consectetur ea asperiores inventore odit sequi, nobis laudantium dicta rem autem nam ipsum, sint facilis magnam quisquam ut tempore quod dolores. Vel consequuntur, est quo voluptatum nobis debitis eaque earum expedita laudantium deserunt distinctio delectus. Necessitatibus quaerat tempora totam, odio eaque ipsum molestiae.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto placeat natus voluptatum quod impedit error delectus, expedita tempore amet explicabo facilis, molestiae. Laboriosam sequi ipsam odio, quos nisi expedita inventore, temporibus officia recusandae excepturi laudantium, molestiae obcaecati, natus. Alias nam reiciendis pariatur neque, voluptatibus illum, eum incidunt quas odio ad, omnis adipisci, eaque. Earum, et, mollitia repudiandae odit, voluptatem minus delectus nihil adipisci porro accusantium similique consequuntur! Labore quia facilis autem itaque nesciunt, beatae nulla nostrum provident pariatur dolores inventore. Soluta quo placeat fuga, harum beatae possimus atque consequuntur ab earum ipsum minus, dolorum tenetur perspiciatis architecto officiis quisquam reprehenderit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptates accusamus, laboriosam ea beatae repudiandae vero, assumenda inventore ut. Ad veritatis, facilis aperiam reprehenderit iure laboriosam ab mollitia expedita magni. Nam dolore voluptas pariatur, nemo repellat commodi ipsam laborum tempore, incidunt eaque fugit aliquid, voluptatem dicta ad atque ex numquam reiciendis consequatur harum tenetur nobis sapiente odio. Sed, a, culpa laboriosam, perspiciatis dolorem nihil voluptatum tenetur alias ipsum, sint suscipit.</p>
+      <p>Perferendis, quibusdam, culpa! Dolorem cum maxime consectetur. Molestias libero quidem porro voluptate ex est reprehenderit eligendi a, nesciunt velit tempore quis corporis quasi, dolorum tempora quaerat. Itaque delectus, tempora. Magnam veritatis totam, laborum nulla, autem ipsam, assumenda id vitae, maxime obcaecati error ad dicta! Sit, officia, incidunt. Suscipit eveniet, sequi nemo animi, quo quaerat numquam saepe consequuntur mollitia eligendi dolorem culpa vero deleniti nisi earum magnam dolor, libero nulla, magni.</p>
+      <p>Quod quia dolorum distinctio esse libero tempora repellat laboriosam minima, qui ratione, expedita temporibus aperiam iusto perferendis iste! Ratione cumque exercitationem modi facilis mollitia quas ut nesciunt nostrum veniam quia nobis placeat vitae fugiat quam, voluptatum ipsam assumenda. Facere, autem cum vero ab, facilis et rem illo consequuntur maiores, quod dolorem pariatur velit! Possimus saepe reiciendis, repellendus blanditiis, dolorum unde aliquid quisquam. Pariatur dolorem nisi fugiat doloribus perferendis laudantium quia?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum atque pariatur aliquam? Error, voluptatum quas iure eligendi laborum accusantium deleniti pariatur assumenda, molestiae repudiandae magni! Dolorem maiores qui exercitationem doloribus?</p>
+      <p>Et labore inventore at ut aperiam adipisci rem ipsam, placeat vero ratione, fugiat voluptate eos voluptas sunt ipsum architecto ab libero culpa tempore in quo consectetur a reprehenderit. Dolorum, magnam.</p>
+      <p>Alias, excepturi! Nulla magni voluptas eos natus quo perspiciatis nesciunt. Soluta maxime possimus magnam, voluptates natus, voluptatum unde saepe? Magnam, vitae, id debitis veniam possimus amet tempore consectetur sapiente neque!</p>
+      <p>Magni incidunt ipsam ducimus nisi iure earum aliquid mollitia, officiis repellat atque, ipsum! Velit enim laboriosam inventore asperiores eaque omnis sit, repellendus minus labore earum aliquam expedita dolor quia nesciunt!</p>
+      <p>Quaerat quis, impedit vitae itaque unde quidem aspernatur perferendis beatae fugiat cum est molestias animi facilis maxime laudantium possimus, voluptatem earum aliquam quae obcaecati explicabo eveniet vero facere accusantium voluptates.</p>
+      <p>Sit, inventore perferendis soluta laborum labore sed excepturi. Quaerat architecto, ipsum culpa possimus! Incidunt assumenda et neque ullam reprehenderit delectus, error, voluptate recusandae explicabo quia quidem libero impedit sapiente fugit!</p>
+      <p>Quas sit architecto dignissimos molestiae magnam reiciendis, ad nostrum voluptates nulla, dolorum facere dolor harum dolorem nisi magni eveniet, sapiente culpa. Quia provident eligendi neque nemo debitis velit quas ipsum.</p>
+      <p>Ullam vero architecto animi, quasi itaque aliquam accusamus magni dicta est! Nemo aperiam dignissimos, porro voluptatum rerum ea dolore dolores quisquam nihil, dolorum eaque sapiente quam laboriosam harum nisi voluptatem.</p>
+      <p>Ab inventore nesciunt sequi eius sed perspiciatis unde atque ratione veritatis sit, at illum impedit iste iure tempora explicabo officia quia, assumenda perferendis accusantium officiis. Et assumenda odio ut fuga!</p>
+      <p>Autem modi deserunt suscipit eligendi laborum error voluptatum omnis aliquam enim sed in, repudiandae fugiat recusandae animi placeat odit harum itaque deleniti labore pariatur asperiores. Mollitia nam illo, quae adipisci.</p>
+    </div>
+
+  </article>
+</template>
+<script>
+export default {
+  name: 'post',
+  mounted () {
+    this.$el.addEventListener('scroll', function(){
+    let scrollTop = this.$el.scrollTop;
+    let c_height = this.$el.clientHeight;
+    let s_height = this.$el.scrollHeight;
+
+    let scrollPercent = Math.round((scrollTop) / (s_height - c_height) * 100);
+
+    this.$emit('progressUpdate', scrollPercent);
+    }.bind(this))
+  }
+}
+</script>
+<style>
+  article{
+    width: 100%;
+    flex: 1 1 100%;
+    max-height: calc(100vh - 80px);
+    background-color: #eee;
+    overflow: scroll;
+  }
+  article .content{
+    padding: 25px;
+  }
+  article .content h2 {
+    color: ##11224F;
+    font-size: 32px;
+    font-weight: 900;
+    margin-bottom: 10px;
+  }
+  article .content h3{
+    color: #676767;
+    font-size: 22px;
+    font-weight: 300;
+    margin-bottom: 30px;
+  }
+  article .content p{
+color: #313131;
+font-size: 18px;
+font-weight: 300;
+line-height: 1.5;
+margin-bottom: 15px;
+  }
+</style>
